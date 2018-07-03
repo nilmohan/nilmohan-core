5fd8fb77ab141e67bed95052bf9ed96548c91d91
nilmohan <nilmohan@cisco.com>
2018-07-03 10:29:00 +0530

    remove security info

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,6 +1,6 @@
 import * as firebase from 'firebase';
 
-/*
+
 const config = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
@@ -9,15 +9,8 @@ const config = {
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
-*/
-var config = {
-  apiKey: "AIzaSyAc_ug_bqQn77WXaayKrLa_ocbxEIS_Xao",
-  authDomain: "testing-54e0d.firebaseapp.com",
-  databaseURL: "https://testing-54e0d.firebaseio.com",
-  projectId: "testing-54e0d",
-  storageBucket: "",
-  messagingSenderId: "268765048986"
-};
+
+
 
 firebase.initializeApp(config);
 
